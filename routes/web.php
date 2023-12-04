b2ddba4c2773a48e9820092f4c88ee231f4507d0
dianaalbs <dianaadilla@student.uns.ac.id>
2023-12-04 14:03:30 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\TestDatabase;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,14 @@ Route::get('/', function () {
 });
 
 Route::get('/test', [TestDatabase::class, 'test']);
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
