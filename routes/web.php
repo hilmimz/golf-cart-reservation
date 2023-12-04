06ffd918d0539209ec95a83561db7fe11104dac3
dianaalbs <dianaadilla@student.uns.ac.id>
2023-12-04 14:43:22 +0700

    aaa

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,5 @@ Route::get('/landing', function () {
 Route::get('/test', [TestDatabase::class, 'test']);
 
 Route::get('/register', [RegisterController::class, 'index']);
-Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
