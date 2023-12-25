2ae93fbcf400584727523cee1e44dcc4ad69b08a
MuhDaffa87 <rajinbelajar87@gmail.com>
2023-12-25 19:15:56 +0700

    update daffa2

--- resources/views/admin/kelola_sopir.blade.php
+++ resources/views/admin/kelola_sopir.blade.php
@@ -81,7 +81,7 @@
     <!-- Tabel -->
     <div class="row">
       <div class="col-12 col-sm-12">
-        <table class="table-responsive mt-4 table-hover rounded-corners">
+        <table class="table-responsive mt-4 mb-5 table-hover rounded-corners">
           <thead class="shadow-sm" style="background-color:white">
             <!-- Judul Tabel -->
             <tr class="rounded-3 text-center p-3">
@@ -203,7 +203,137 @@
             @endforelse  --}}
 
           </tbody>
-          
+
+          {{---------------------- TABLE SUPIR UNAKTIFFFF ---------------------}}
+        </table>
+        <div class="row mt-3">
+            <div class="col-lg-8 offset-lg-2 col-sm-8 offset-sm-2 text-center div-title-admin" style="font-weight: 900">
+              <h4 class="p-2">Sopir Tidak Aktif</h4>
+            </div>
+          </div>
+      </div>
+      <div class="col-12 col-sm-12">
+        <table class="table-responsive table-hover rounded-corners">
+          <thead class="shadow-sm" style="background-color:white">
+            <!-- Judul Tabel -->
+            <tr class="rounded-3 text-center p-3">
+              <th class="col-5 p-3 col-sm-5 div-title-admin" style="font-size:18px">Pertanyaan</th>
+              <th class="col-3 p-3 col-sm-3 div-title-admin" style="font-size:18px">Jawaban</th>
+              <th class="col-2 p-3 col-sm-2 div-title-admin" style="font-size:18px">Skor</th>
+              <th class="col-2 p-3 col-sm-2 div-title-admin" style="font-size:18px">Aksi</th>
+            </tr>
+          </thead>
+
+          <tbody style="font-size:14px">
+            
+            {{-- @forelse($pertanyaan as $pertanyaan) --}}
+              <tr class="shadow-sm" style="background-color:white">
+
+                <!-- Isi Pertanyaan -->
+                <td class="p-3">
+                  {{-- <a href="{{ url('setting-jawaban/'.$pertanyaan->id) }}" style="font-size:15px">
+                    {{ $pertanyaan->pertanyaan }}
+                  </a> --}}asu
+                </td>       
+                
+                <!-- Isi Jawaban-->
+                <td class="p-3 fw-bold">
+                  <div class="input-group input-group-sm my-lg-1 col-sm-12 justify-content-center">
+                    {{-- @forelse($pertanyaan->jawaban as $jawaban) --}}
+                      <div class="col-12 text-center shadow my-2 p-2" style="background-color:white; font-size:15px; font-weight:500">
+                        {{-- {{ $jawaban->jawaban }} --}}asu
+                      </div>
+                    {{-- @empty
+                      <p>tidak ada record</p>
+                    @endforelse --}}
+                  </div>
+                </td>
+
+                <!-- Isi Skor-->
+                <td class="p-3 fw-bold">
+                  <div class="input-group input-group-sm my-lg-1 col-sm-12 justify-content-center">
+                    {{-- @forelse($pertanyaan->skor as $skor) --}}
+                      <div class="col-12 text-center shadow my-2 p-2" style="background-color:white; font-size:15px; font-weight:500">
+                        {{-- {{ $skor->skor }} --}}asu
+                      </div>
+                    {{-- @empty
+                      <p>tidak ada record</p>
+                    @endforelse                      --}}
+                  </div>
+                </td>
+              
+                <!-- Button Edit dan Hapus pertanyaan -->
+                <td class="p-lg-3 p-sm-3 text-center">
+                  <button type="button" class="btn btn-success col-lg-10 col-sm-12 mx-lg-2 my-2 rounded-3">
+                    <i class="fa fa-check" style="font-size: 20px"></i>        
+                  </button>
+                  <button type="button" data-toggle="modal" data-target="#HapusModal" class="btn btn-danger col-lg-10 col-sm-12 mx-lg-2 my-2 rounded-3" >
+                    <i class="fa fa-trash" style="font-size: 20px"></i>
+                  </button>  
+                </td>
+            
+              </tr>
+
+              {{-- modal konfirmasi hapus --}}
+              <div class="modal fade" id="HapusModal" tabindex="-1" aria-labelledby="HapusModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <form method="post" action="{{url('admin-setting/delete/')}}">
+                      @csrf
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="HapusModalLabel">Konfirmasi Hapus</h5>
+                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+                          {{-- <span aria-hidden="true">&times;</span> --}}
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        Apakah anda yakin ingin menghapus pertanyaan "asu"?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Ya</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Tidak</button>
+                      </div>
+                    </form>             
+                  </div>
+                </div>
+              </div>
+
+              {{-- modal update --}}
+              <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <form method="post" action="{{url('admin-setting/update/')}}">
+                      @csrf
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">Update Sopir</h5>
+                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+                          {{-- <span aria-hidden="true">&times;</span> --}}
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <div class="form-group">
+                          <label for="pertanyaan">Pertanyaan</label>
+                          <input type="text" value="" class="form-control" id="pertanyaan" name="pertanyaan" required>
+                          {{-- @if($errors->has('pertanyaan'))
+                            <div class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('pertanyaan') }}</strong>
+                            </div>
+                          @endif --}}
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+                      </div>
+                    </form>             
+                  </div>
+                </div>
+              </div>
+            
+            {{-- @empty
+              <p>tidak ada record</p>
+            @endforelse  --}}
+          </tbody> 
         </table>
       </div>
     </div>
