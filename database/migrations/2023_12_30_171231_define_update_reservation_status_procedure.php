abe665e23f2c0ae20d67e72add4b41a8b835fed8
Hilmi Muzhaffar <hilmimuzhaffar15@student.uns.ac.id>
2023-12-30 17:48:51 +0700

    migration update: define update reservation status procedure and create job for it minutely

---
+++ database/migrations/2023_12_30_171231_define_update_reservation_status_procedure.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+            CREATE OR REPLACE PROCEDURE update_reservation_status AS
+            BEGIN
+            -- Jika waktu sekarang sudah lebih dari 5 menit dari waktu keberangkatan maka status menjadi 0
+            UPDATE reservations r
+            SET r.status = 0
+            WHERE EXISTS (
+                SELECT 1
+                FROM schedules s
+                WHERE s.id = r.route_start
+                AND EXTRACT(HOUR FROM (SYSTIMESTAMP - s.time)) * 60 +
+                    EXTRACT(MINUTE FROM (SYSTIMESTAMP - s.time)) > 5
+            );
+            END update_reservation_status;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP PROCEDURE update_reservation_status');
+    }
+};
---
+++ database/migrations/2023_12_30_171535_run_update_reservation_status_every_minute.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared("
+            DECLARE
+                job_exists NUMBER;
+            BEGIN
+                -- Memeriksa apakah job sudah ada
+                SELECT COUNT(*)
+                INTO job_exists
+                FROM user_scheduler_jobs
+                WHERE job_name = 'UPDATE_RESERVATION_STATUS_JOB';
+        
+                IF job_exists = 0 THEN
+                    DBMS_SCHEDULER.create_job (
+                        job_name        => 'UPDATE_RESERVATION_STATUS_JOB',
+                        job_type        => 'PLSQL_BLOCK',
+                        job_action      => 'BEGIN update_reservation_status_proc; END;',
+                        start_date      => SYSTIMESTAMP,
+                        repeat_interval => 'FREQ=MINUTELY; INTERVAL=1',
+                        enabled         => TRUE
+                    );
+                END IF;
+            END;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared("
+        BEGIN
+            DBMS_SCHEDULER.drop_job('UPDATE_RESERVATION_STATUS_JOB');
+        END;
+      ");
+    }
+};
