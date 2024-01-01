696c132be6dacfed6acffc3a542f488e68f2bccf
asyahra19 <asyahrara02@student.uns.ac.id>
2024-01-01 15:05:59 +0700

    fixing

--- resources/views/admin/kelola_golfcart.blade.php
+++ resources/views/admin/kelola_golfcart.blade.php
@@ -9,7 +9,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="detailModalLabel">TAMBAH Golf Cart</h5>
+                <h5 class="modal-title" id="detailModalLabel">Tambah Golf Cart</h5>
                 <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
